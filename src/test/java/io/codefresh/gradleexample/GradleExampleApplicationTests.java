770a73526c119038150004d019914446fd2ded0c
Kostis Kapelonis <kostis@codefresh.io>
2019-05-14 17:55:38 +0300

    Added dummy unit test

--- src/test/java/io/codefresh/gradleexample/GradleExampleApplicationTests.java
+++ src/test/java/io/codefresh/gradleexample/GradleExampleApplicationTests.java
@@ -1,5 +1,7 @@
 package io.codefresh.gradleexample;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,7 @@ public class GradleExampleApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertEquals("Expected correct message","Hello World","Hello "+"World");
 	}
 
 }
